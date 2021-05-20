656a9b2256332cea27cbe573f511ef36e0db893e
2633-code <2633931483@qq.com>
2021-05-20 20:04:58 +0800

    ljh

---
+++ IOT.Core.Model/ViewOrderInfoAndCommodity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOT.Core.Model
+{
+    public class ViewOrderInfoAndCommodityCommodity
+    {
+        public int Orderid { get; set; }
+        public int CommodityId { get; set; }
+        public int UserId { get; set; }
+        public int SendWay { get; set; }
+        public int OrderState { get; set; }
+        public string Remark { get; set; }
+        public float CommodityPrice { get; set; }
+        public float DistributionCosts { get; set; }
+        public float OrderPrice { get; set; }
+        public float CouponPrice { get; set; }
+        public float AmountPaid { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public string CommodityName { get; set; }//商品名称
+        public string CommodityPic { get; set; } //商品图片
+        public string ShopPrice { get; set; }    //销售单价
+        public int SId { get; set; }             //规格
+
+        public string UserName { get; set; }    //买家姓名
+        public string Phone { get; set; }    //买家电话
+        public string Address { get; set; }    //买家地址
+
+        
+            
+            
+    }
+}
